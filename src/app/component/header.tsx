77cf18e7aff37e0a3a4a6f7777aee7e0235bbf00
Angelicabassey <angelicabassey44@gmail.com>
2024-03-18 16:46:40 +0100

    fixed: i just fix some errors

--- src/app/component/header.tsx
+++ src/app/component/header.tsx
@@ -1,43 +1,37 @@
 import Image from 'next/image';
 import Arrowimg from "../component/arrowimg";
 
-
 export default function Header() {
     return (
         <main className="border bg-white">
-            <div className="flex- justify-between- flex-wrap px-16 text-wrap">
+            <div className="justify-between- flex-wrap px-16 text-wrap">
                 <div className="px-16- flex justify-between items-center">
-                  <a href="./home">
-                  <div className="">
-                        <Image
-                            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                            src="/logo.png"
-                            alt="Next.js Logo"
-                            width={150}
-                            height={150}
-                            priority
-                        />
-                    </div>
-                  </a>
+                    <a href="./home">
+                        <div className="">
+                            <Image
+                                className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+                                src="/logo.png"
+                                alt="Next.js Logo"
+                                width={150}
+                                height={150}
+                                priority
+                            />
+                        </div>
+                    </a>
                     <div className="flex items-center gap-20">
                         <p><a href="">Home</a></p>
                         <p><a href="">About Us</a></p>
                         <p><a href="">Roadmap</a></p>
                         <p><a href="">Contact Us</a></p>
-
                     </div>
-                    <div className=" bg-main px-6 py-2 rounded-md">
+                    <div className="bg-main px-6 py-2 rounded-md">
                         <a className='flex items-center gap-2' href="">
-                            <p className='rounded-md px-6- font-normal py-2- xl:text-base sm:text-sm text-white'>Let's Earn</p>
+                            <p className='rounded-md px-6- font-normal py-2- xl:text-base sm:text-sm text-white'>Let&apos;s Earn</p>
                             <Arrowimg />
                         </a>
                     </div>
                 </div>
-
             </div>
-
         </main>
-
     )
 }
-
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons'
@@ -10,37 +10,31 @@ import Testimonial from "../component/testimonial";
 import Platform from "../component/platform";
 import Footer from "../component/footer";
 
-
-
-
-
 export default function Home() {
-  return (
-    <main className="bg-herobg">
-      <div className="flex items-center justify-between px-16 py-16">
-        <div className="flex flex-col gap-6 w-2/4">
-          <h3 className=" font-medium text-main text-5xl w-8/12-">Discover Solana: <br />Learn, <a className="text-primarytext" href="">Earn</a> and Thrive <br /> in the Web3 Economy</h3>
-          <p className="w-94 font-semibold text-sm">Lorem ipsum dolor sit amet consectetur. Vel blandit tempor id nunc et auctor tellus eu nisi. Eu sem tellus magna vulputate. Neque etiam sit molestie aenean placerat ornare quis. Urna duis morbi sit pretium et.</p>
-          <div className=" bg-main px-6 py-2 rounded-md w-4/12 ">
-            <a className='flex items-center gap-2- text-center' href="./">
-              <p className='px-5 font-normal py-2- text-center xl:text-base sm:text-sm text-white'>Let's Earn</p>
-              <Arrowimg />
-            </a>
-          </div>
-        </div>
-        <div>
-          <Sideimg />
-        </div>
-      </div>
-      <Service/>
-      <Testimonial/>
-      {/* <question/> */}
-      <Platform/>
-      <div className="my-20">
-      <Footer/>
-
-      </div>
-    </main>
-  )
+    return (
+        <main className="bg-herobg">
+            <div className="flex items-center justify-between px-16 py-16">
+                <div className="flex flex-col gap-6 w-2/4">
+                    <h3 className=" font-medium text-main text-5xl">Discover Solana: <br />Learn, <a className="text-primarytext" href="">Earn</a> and Thrive <br /> in the Web3 Economy</h3>
+                    <p className="w-94 font-semibold text-sm">Lorem ipsum dolor sit amet consectetur. Vel blandit tempor id nunc et auctor tellus eu nisi. Eu sem tellus magna vulputate. Neque etiam sit molestie aenean placerat ornare quis. Urna duis morbi sit pretium et.</p>
+                    <div className="bg-main px-6 py-2 rounded-md w-4/12">
+                        <a className='flex items-center text-center' href="">
+                            <p className='px-5 font-normal text-center xl:text-base sm:text-sm text-white'>Let&apos;s Earn</p>
+                            <Arrowimg />
+                        </a>
+                    </div>
+                </div>
+                <div>
+                    <Sideimg />
+                </div>
+            </div>
+            <Service />
+            <Testimonial />
+            {/* <question/> */}
+            <Platform />
+            <div className="my-20">
+                <Footer />
+            </div>
+        </main>
+    )
 }
-
